30e7c91caea4de41948851da4d28869ede7e2c00
Milvio LuA <milviolua@gmail.com>
2026-01-16 20:32:30 +0700

    update build

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,17 +1,19 @@
 ===============create and publish a python package===============
 
-cd suite_identity/milvio_redis_package
+cd milvio_package
 
-python3 -m venv venv
-source venv/bin/activate
+# Use existing venv or create new one
+# python3 -m venv .venv
+# source .venv/bin/activate
 pip install build twine
 
+# Build
+rm -rf dist build *.egg-info
 python3 -m build
 
+# Publish to PyPI
+# You will be prompted for your username (__token__) and password (your PyPI API token)
 twine upload dist/*
 
-
-
-====================================
 
 
